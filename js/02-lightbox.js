8ab7898b471349691fe6e11b79537428ad974abb
Veronika0136 <Artemova331@gmail.com>
2023-01-13 19:10:44 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+console.log();
+
+const markup = galleryItems.reduce((acc,{
+    preview,
+    original,
+    description})=> 
+    acc + `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" 
+    src="${preview}" 
+    alt="${description}"/>
+  </a>`, '');
+
+console.log ();
+
+galleryList.insertAdjacentHTML('beforeend', markup)
+galleryList.addEventListener('click', OpenImage)
+
+function OpenImage (evt){
+   evt.preventDefault();  
+  console.dir(evt.target);
+  console.dir(evt.currentTarget);
+
+
+}
