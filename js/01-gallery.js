c3ad12c993f55ef7dc91d5650334779f9dd66058
Veronika0136 <Artemova331@gmail.com>
2023-01-13 19:05:24 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,36 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+console.log();
+
+const markup = galleryItems.reduce((acc,{
+    preview,
+    original,
+    description})=> 
+    acc + `<div class="gallery__item">
+   <a class="gallery__link" href="${original}">
+   <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+    </a>
+   </div>`, '');
+
+console.log ();
+
+galleryList.insertAdjacentHTML('beforeend', markup)
+galleryList.addEventListener('click', OpenImage)
+
+function OpenImage (evt){
+   evt.preventDefault();  
+  console.dir(evt.target);
+  console.dir(evt.currentTarget);
+
+
+}
+
+
